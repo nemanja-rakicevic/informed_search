7521e29921e03113b0526203ec3bd4fb7fa5c562
nemanja-rakicevic <n.rakicevic@imperial.ac.uk>
2020-06-20 20:20:06 +0100

    Added experiment config files; renamed CAD directory

--- informed_search/main_training.py
+++ informed_search/main_training.py
@@ -7,6 +7,7 @@ Description:
 """
 
 import os
+import sys
 import json
 import time
 import logging
@@ -19,12 +20,16 @@ logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
+
+
+parser.add_argument('--config_file', required=True)
+
 # Experiment
 parser.add_argument('-s', '--seed', default=100,      
                     type=int, help="Experiment seed.")
-parser.add_argument('-ntrial', '--num-trial', default=300,  
+parser.add_argument('-ntrial', '--num_trial', default=300,  
                     type=int, help="Number of trials to run.")
-parser.add_argument('-efreq', '--eval-freq', default=10,  
+parser.add_argument('-efreq', '--eval_freq', default=10,  
                     type=int, help="How often to evaluate on test set.")
 
 # Task
@@ -37,7 +42,7 @@ parser.add_argument('-e', '--environment', default='sim5link',
                          "'sim5link'\n"
                          "'robot'")
 # Modelling
-parser.add_argument('-m', '--search-type', default='UIDF', 
+parser.add_argument('-m', '--search_type', default='UIDF', 
                     help="Select which model to use: "
                          "random\n"
                          "informed\n"
@@ -45,18 +50,18 @@ parser.add_argument('-m', '--search-type', default='UIDF',
                          "entropy\n"
                          "BO")
 
-parser.add_argument('-pcov', '--pidf-cov', default=0.1, 
+parser.add_argument('-pcov', '--pidf_cov', default=0.1, 
                     type=float,  
                     help="Penalisation function covariance coefficient.")
 
-parser.add_argument('-kname', '--kernel-name', default='se',      
+parser.add_argument('-kname', '--kernel_name', default='se',      
                     help="Gaussian Process Regression kernel function.")
 
-parser.add_argument('-kls', '--kernel-lenscale', default=0.01,      
+parser.add_argument('-kls', '--kernel_lenscale', default=0.01,      
                     type=float,
                     help="Sigma coefficient of the kernel")
 
-parser.add_argument('-ksig', '--kernel-sigma', default=1.,      
+parser.add_argument('-ksig', '--kernel_sigma', default=1.,      
                     type=float, 
                     help="Sigma coefficient of the kernel")
 
@@ -75,21 +80,43 @@ parser.add_argument('-v', '--verbose', default=0,
                              "3: training and test detailed info\n")
 
 
-def _start_logging():
+def _load_args():
+    """ 
+        Load experiment args from config and overwrite from cmd line 
+    """
+    args = parser.parse_args()
+    with open(args.config_file) as json_file:
+        metadata = json.load(json_file)
+    metadata['config_file'] = args.config_file
+    # additional args to overwrite file
+    if len(sys.argv) > 3:
+        args = vars(args)
+        for a in sys.argv[3:]:
+            if '--' in a:
+                k = a[2:]
+                # k is the actual key
+                if k in metadata.keys():
+                    metadata[k] = args[k]
+    return metadata
+
+
+def _start_logging(taskargs):
     """
         Create the experiment directory and start logging
     """
-    args = parser.parse_args()
-    dirname = "experiment_data/simulation/" \
+    # Create experiment directory
+    dirname = os.path.join(os.getcwd(), 'experiment_data', 'simulation')
+    dirname = os.path.join(dirname,
                 "ENV_{}__SEARCH_{}_res{}_cov{}_kernelSE_sl{}__{}_{}".format(
-                args.environment,
-                args.search_type, 
-                args.resolution, 
-                args.pidf_cov, args.kernel_sigma, args.seed,
-                datetime.datetime.today().strftime("%Y-%m-%d_%H-%M-%S"))
+                taskargs['environment'],
+                taskargs['search_type'], 
+                taskargs['resolution'], 
+                taskargs['pidf_cov'], 
+                taskargs['kernel_sigma'], 
+                taskargs['seed'],
+                datetime.datetime.today().strftime("%Y-%m-%d_%H-%M-%S")))
     os.makedirs(dirname)
-    task_kwargs = vars(args)
-    task_kwargs['dirname'] = dirname
+    taskargs['dirname'] = dirname
     # Start logging info
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(name)-25s '
@@ -102,14 +129,15 @@ def _start_logging():
     # Save the modified arguments
     filename = '{}/experiment_metadata.json'.format(dirname)
     with open(filename, 'w') as outfile:  
-            json.dump(task_kwargs, outfile, sort_keys=True, indent=4)
+            json.dump(taskargs, outfile, sort_keys=True, indent=4)
     logger.info('Starting session: {}\n'.format(dirname))
-    return task_kwargs
+    return taskargs
 
 
 def main_run():
     # Initialise
-    task_kwargs = _start_logging()
+    task_kwargs = _load_args()
+    task_kwargs = _start_logging(task_kwargs)
     experiment = expm.ExperimentManager(task_kwargs)
     # Run main loop
     for ntrial_ in range(1, task_kwargs['num_trial']+1):         
@@ -124,7 +152,7 @@ def main_run():
 
 if __name__ == "__main__":
     try:
-            main_run()
+        main_run()
     except Exception as e:
-            logging.fatal(e, exc_info=True)
+        logging.fatal(e, exc_info=True)
 
