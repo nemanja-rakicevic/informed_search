d52f219a24d98db34c49ffb93403d7b35670c859
nemanja-rakicevic <n.rakicevic@imperial.ac.uk>
2020-09-28 17:26:26 +0100

    Refactored evaluation scripts

--- informed_search/load_full_test.py
+++ informed_search/load_full_test.py
@@ -3,7 +3,8 @@
 Author:         Nemanja Rakicevic
 Date  :         June 2020
 Description:
-                Load the model from a file and perform a full test
+                Load a model from the experiment directory and evaluate it on
+                the full test set.
 """
 
 
@@ -12,14 +13,13 @@ import argparse
 import numpy as np
 
 import utils.experiment_manage as expm
-from utils.misc import _TAB
 
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('-load', '--load_path',
                     default=None, required=True,
                     help="Path to the learned model file.")
-
 
 
 def load_metadata(datapath):
@@ -29,40 +29,39 @@ def load_metadata(datapath):
     return args_dict
 
 
-
-def main_test(load_path, display=True):
-
+def main_test(load_path):
+    """ Run model evaluation on full test set """
     load_task_kwargs = load_metadata(load_path)
-    experiment = expm.ExperimentManager(load_task_kwargs, 
-                                        load_model_path=load_path)
-
-    test_stats = experiment.evaluate_test_cases(num_trial=None, 
-                                                save_model=False, 
-                                                save_test_progress=False,
-                                                verbose=True,
-                                                save_plots=False, 
-                                                save_data=False)
-
-
-    errors_all = np.array([[x['euclid_error'], x['polar_error']] \
-                                for x in test_stats])
+    experiment = expm.ExperimentManager(
+        load_task_kwargs,
+        load_model_path=load_path)
+    test_stats = experiment.evaluate_test_cases(
+        num_trial=None,
+        save_model=False,
+        save_test_progress=False,
+        verbose=True,
+        save_plots=False,
+        save_data=False)
+    # Extract information
+    errors_all = np.array([[x['euclid_error'], x['polar_error']]
+                           for x in test_stats])
     errors_mean = errors_all.mean(axis=0)
-    errors_std  = errors_all.std(axis=0)
+    errors_std = errors_all.std(axis=0)
     num_total = len(test_stats)
-    num_fails = sum([x['fail_status']>0 for x in test_stats])
-    num_success = num_total-num_fails
+    num_fails = sum([x['fail_status'] > 0 for x in test_stats])
+    num_success = num_total - num_fails
     model_error_mean = np.mean([x['model_polar_error'] for x in test_stats])
-
-    # Log test results
+    # Print outcome info
     print("\n{} TESTING {} cases {}"
           "\n - Sucessful/Failed:       {} / {} ({})"
           "\n - Model polar error mean: {:4.2f}"
           "\n - Euclidian error mean:   {:4.2f}"
           "\n - Polar error norm mean:  {:4.2f}\n{}".format(
-            '-'*15, num_total, '-'*15, num_success, num_fails, 
-            num_total, model_error_mean, errors_mean[0],
-            errors_mean[1], '-'*50))
-
+              '-' * 15, num_total, '-' * 15,
+              num_success, num_fails,
+              num_total, model_error_mean,
+              errors_mean[0], errors_mean[1],
+              '-' * 50))
 
 
 if __name__ == "__main__":
--- informed_search/load_target_test.py
+++ informed_search/load_target_test.py
@@ -3,7 +3,8 @@
 Author:         Nemanja Rakicevic
 Date  :         January 2018
 Description:
-                Load the model from a file and perform a user defined test
+                Load a model from the experiment directory and evaluate it on a
+                user defined test.
 """
 
 
@@ -11,58 +12,60 @@ import json
 import argparse
 
 import utils.experiment_manage as expm
-from utils.misc import _TAB
 
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('-load', '--load_path',
                     default=None, required=True,
                     help="Path to the learned model file.")
 
+parser.add_argument('-disp', '--display',
+                    default=False,
+                    help="Show execution.")
 
 
 def load_metadata(datapath):
-    """Extract class experiment metadata """
+    """ Extract class experiment metadata """
     with open(datapath + '/experiment_metadata.json', 'r') as f:
         args_dict = json.load(f)
     return args_dict
 
 
-
-def main_test(load_path):
-
+def main_test(load_path, display):
+    """ Run model evaluation on test targets """
     load_task_kwargs = load_metadata(load_path)
-    experiment = expm.ExperimentManager(load_task_kwargs, 
-                                        load_model_path=load_path)
-
+    experiment = expm.ExperimentManager(
+        load_task_kwargs, 
+        load_model_path=load_path)
+    # Allow evaluation of multiple test positions
     while True:
         # Input target position
         while True:
             try:
-                angle_s, dist_s = input("\nEnter TARGET: "
-                                    "angle [deg], distance[m] >> ").split(",")
+                angle_s, dist_s = input(
+                    "\nEnter TARGET: angle [deg], distance[m] >> ").split(",")
                 test_target = [float(angle_s), float(dist_s)]
                 break
             except Exception as i:
                 print(i)
                 continue
-        _, _, test_stats = experiment.evaluate_single_test(test_target,
-                                                           display=False,
-                                                           verbose=True)
-
+        # Evaluate given test position
+        _, _, test_stats = experiment.evaluate_single_test(
+            test_target, display=display, verbose=True)
+        # Print outcome info
         print("{} TEST TARGET (angle: {}; distance: {}) {}"
               "\n - Trial outcome:     {} [{}]; ball ({:4.2f},{:4.2f})"
               "\n - Model polar error: {:4.2f}"
               "\n - Euclidian error:   {:4.2f}"
               "\n - Polar error norm:  {:4.2f}\n{}".format(
-                    '-'*7, angle_s, dist_s, '-'*7, 
-                    test_stats['trial_outcome'], test_stats['fail_status'], 
-                    *test_stats['ball_polar'],
-                    test_stats['model_polar_error'], 
-                    test_stats['euclid_error'], 
-                    test_stats['polar_error'], '-'*50))
-
-        # Continue
+                  '-' * 7, angle_s, dist_s, '-' * 7,
+                  test_stats['trial_outcome'], test_stats['fail_status'],
+                  *test_stats['ball_polar'],
+                  test_stats['model_polar_error'],
+                  test_stats['euclid_error'],
+                  test_stats['polar_error'], '-' * 50))
+        # Continue or exit
         if input("\nEXECUTION DONE. "
                  "Enter to try again, or (q) to quit ") == 'q':
             break
@@ -70,4 +73,4 @@ def main_test(load_path):
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main_test(load_path=args.load_path)
+    main_test(load_path=args.load_path, display=args.display)
