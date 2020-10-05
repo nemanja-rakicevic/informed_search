40ad588fa2fd5e1bcce40a5c24dbae4910da1572
nemanja-rakicevic <n.rakicevic@imperial.ac.uk>
2020-10-05 17:42:38 +0100

    Fixed plot limits

--- informed_search/analysis/plot_evaluations.py
+++ informed_search/analysis/plot_evaluations.py
@@ -97,48 +97,60 @@ def plot_performance(load_path,
     f.set_size_inches(f.get_size_inches()[0] * 3, f.get_size_inches()[1] * 2)
 
     # Euclidean error means over full test set
+    max_xaxis = 0
+    max_yaxis = 0
     for i, a in enumerate(all_stats):
         mean = np.array(a['mean'][0])
         std = np.array(a['mean'][1])
         xaxis = range(len(mean))
+        if len(mean) > max_xaxis:
+            max_xaxis = len(mean)
+        if mean.max() > max_yaxis:
+            max_yaxis = mean.max()
         lb = a['model']  # + ' models'
         axarr[0].plot(mean, label=lb, linewidth=lw)
         axarr[0].fill_between(xaxis, (mean - std).clip(0), mean + std,
                               alpha=0.5)  # , label=lb)
     axarr[0].set_ylabel('Test error mean', labelpad=14)  # (Euclidean distance)
     axarr[0].grid(color='gray', linestyle=':', linewidth=0.8)
-    axarr[0].set_xlim(0, min(CUTOFF, len(xaxis)))
-    axarr[0].set_ylim(0, 24)
+    axarr[0].set_xlim(0, min(CUTOFF, max_xaxis))
+    axarr[0].set_ylim(0, max(24, max_yaxis))
     axarr[0].yaxis.set_major_locator(MultipleLocator(5))
 
     # Euclidean error stds over full test set
+    max_yaxis = 0
     for i, a in enumerate(all_stats):
         mean = np.array(a['std'][0])
         std = np.array(a['std'][1])
         xaxis = range(len(mean))
+        if mean.max() > max_yaxis:
+            max_yaxis = mean.max()
         lb = a['model']  # + ' models'
         axarr[1].plot(mean, label=lb, linewidth=lw)
         axarr[1].fill_between(xaxis, (mean - std).clip(0), mean + std,
                               alpha=0.5)  # , label=lb)
     axarr[1].set_ylabel('Test error std', labelpad=14)
     axarr[1].grid(color='gray', linestyle=':', linewidth=0.8)
-    axarr[0].set_xlim(0, min(CUTOFF, len(xaxis)))
-    axarr[1].set_ylim(0, 11)
+    axarr[0].set_xlim(0, min(CUTOFF, max_xaxis))
+    axarr[1].set_ylim(0, max(11, max_yaxis))
     axarr[1].yaxis.set_major_formatter(FormatStrFormatter('%i'))
     axarr[1].yaxis.set_major_locator(MaxNLocator(5))
 
     # Total fail count over full test set
+    max_yaxis = 0
     for i, a in enumerate(all_stats):
         mean = np.array(a['fails'][0])
         std = np.array(a['fails'][1])
         xaxis = range(len(mean))
+        if mean.max() > max_yaxis:
+            max_yaxis = mean.max()
         lb = a['model']  # + ' models'
         axarr[2].plot(mean, label=lb, linewidth=lw)
         axarr[2].fill_between(xaxis, (mean - std).clip(0), mean + std,
                               alpha=0.5)  # , label=lb)
     axarr[2].set_ylabel('Number of failed tests')
     axarr[2].grid(color='gray', linestyle=':', linewidth=0.8)
-    axarr[0].set_xlim(0, min(CUTOFF, len(xaxis)))
+    axarr[0].set_xlim(0, min(CUTOFF, max_xaxis))
     axarr[2].set_ylim(-1, 160)
     axarr[2].set_xlabel('Trial number')
 
